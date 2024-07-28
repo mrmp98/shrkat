04c054cf6298da80564e816b233fc62cf7cc46ca
arefjami <amirmp2332@gmail.com>
2024-07-29 01:22:07 +0330

    back up and acc is not finish

---
+++ سایت فروشگاه/c/EROR.php
@@ -0,0 +1,20 @@
+<?php 
+class eror 
+ {
+    public   function 404 (){
+        // not found
+    }
+    public   function 403 (){
+        // حق دسترسی ندارد 
+    }
+    public   function 401 (){
+
+    }
+    public   function 400 (){}
+    public   function 504 (){}
+    public   function 503 (){}
+    public   function 502 (){}
+    public   function 500 (){}
+    public   function 408 (){}
+    
+ }
