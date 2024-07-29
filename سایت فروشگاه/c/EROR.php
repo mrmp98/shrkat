f61d88ea7a042e5a1933b2d31a4c44cdd1929c06
mrmp98 <amirmp2332@gmail.com>
2024-07-29 16:46:36 +0330

    rams gozari and rams ghoshaei is finish

--- سایت فروشگاه/c/EROR.php
+++ سایت فروشگاه/c/EROR.php
@@ -7,14 +7,14 @@ class eror
     public   function 403 (){
         // حق دسترسی ندارد 
     }
-    public   function 401 (){
-
+    public   function 504 (){
+        // اینترنت ضعیف 
     }
-    public   function 400 (){}
-    public   function 504 (){}
-    public   function 503 (){}
-    public   function 502 (){}
-    public   function 500 (){}
-    public   function 408 (){}
+    public   function 503 (){
+        // بروز رسانی سایت یا سرور 
+    }
+    public   function 408 (){
+        // زمان از زمان پاسخگویی رد شده 
+    }
     
  }
--- سایت فروشگاه/c/amniyat.php
+++ سایت فروشگاه/c/amniyat.php
@@ -14,7 +14,23 @@
     public function csrf (){}
     public function Insecure_File_Uploads (){}
     public function HTTP_Parameter_Pollution (){}
-    public function rams_gozari (){}
- }
-  $e = new qw 
-   $e->xss() ;
+    public function rams_gozari ($data){
+        $method = 'aes-256-gcm';
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
+        $tag = null;
+        $encrypted = openssl_encrypt($data, $method, 'kvwremveroijverajievmevfvfejoiefafa', 0, $iv, $tag);
+        return base64_encode($iv . $tag . $encrypted);
+    }
+    
+    public function ramz_goshaee (string $data){
+        $method = 'aes-256-gcm';
+        $data = base64_decode($data);
+        $ivLength = openssl_cipher_iv_length($method);
+        $iv = substr($data, 0, $ivLength);
+        $tag = substr($data, $ivLength, 16);
+        $encrypted = substr($data, $ivLength + 16);
+        return openssl_decrypt($encrypted, $method, 'kvwremveroijverajievmevfvfejoiefafa', 0, $iv, $tag);
+    }
+}
+
+$e = new qw();
